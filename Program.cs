adf371c685d711a75a186a244c60f2de7dd6e1a7
Viktoria Moskalenko <vicky1234555617@gmail.com>
2024-04-23 14:21:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson2PullRequest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+}
