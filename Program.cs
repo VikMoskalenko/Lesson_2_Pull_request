a5929812cd1ae7694e3ac743af9e44ed13d3c91f
Viktoria Moskalenko <vicky1234555617@gmail.com>
2024-04-23 14:23:12 +0100

    initial commit

--- Program.cs
+++ Program.cs
@@ -10,4 +10,20 @@ namespace Lesson2PullRequest
         }
         
     }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public Person(int id, string name, string lastName)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
+        public string Greeting(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
 }
