a9a13140311523278a6214c1161d76ed48b4550e
dnaum <geoskill123@gmail.com>
2024-04-25 17:16:26 +0200

    Revert "Naumenko Danylo Git HomeWork"
    This reverts commit 2e6dd49e69ff9690f58cb33be487353b7500113b.

--- Program.cs
+++ Program.cs
@@ -7,10 +7,6 @@ namespace Lesson2PullRequest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-
-            var a = 5;
-            Console.WriteLine(a);
         }
         
     }
