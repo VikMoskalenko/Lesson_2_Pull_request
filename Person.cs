6cf759ce7936c9ce30678f9bbfca1a6c9e48498d
Danylo Naumeko <d.naumenko1111@gmail.com>
2024-04-28 12:54:20 +0200

    Danylo Naumenko. Moving the class Person from class Program to a separate file. Resolving the conflict between the existence of two classes "Person"

--- Person.cs
+++ Person.cs
@@ -10,6 +10,22 @@ namespace Lesson_2_Pull_request
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Age { get; set; }
+        public string LastName { get; set; }
+        public Person(int id, string name, string lastName)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
+        public string Greeting(string name)
+        {
+            return $"Hello, {name}";
+        }
+        public string HowAreYouDoing(string name)
+        {
+            return $"How are you doing, {name}?";
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -20,26 +20,5 @@ namespace Lesson2PullRequest
         }
         
     }
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public Person(int id, string name, string lastName)
-        {
-            Id = id;
-            Name = name;
-            LastName = lastName;
-        }
-        public string Greeting(string name)
-        {
-            return $"Hello, {name}";
-        }
-        public string HowAreYouDoing (string name)
-        {
-            return $"How are you doing, {name}?";
-        }
-
-            
-    }
+    
 }
